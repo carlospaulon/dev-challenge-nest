9f6fd3422741fd262cbd85f71dc199b5d9fe1c1b
Carlos <paulonluis222@gmail.com>
2025-08-10 21:16:11 -0300

    feat(api): add create and list endpoints with DTOs and service methods

--- backend-challenges/src/contact/contact.controller.ts
+++ backend-challenges/src/contact/contact.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { CreateContactDto } from './dto/create-contact.dto';
+import { ContactService } from './contact.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('contact')
-export class ContactController {}
+export class ContactController {
+    // Constructor
+    constructor(private readonly contactService: ContactService) { }
+
+    @Post('create_contact')
+    @HttpCode(200)
+    @ApiOperation({ summary: 'Cria um novo contato' })
+    @ApiResponse({
+        status: 200,
+        description: 'Contato criado com sucesso',
+        // schema: {
+        //     example: { confirmation: `` }
+        // }
+    })
+    async create(@Body() createContactDto: CreateContactDto) {
+        return this.contactService.createContact(createContactDto);
+    }
+
+    @Get('contacts')
+    @ApiOperation({ summary: 'Lista todos os contatos criados' })
+    @ApiResponse({
+        status: 200,
+        description: 'Lista de contatos',
+        type: [CreateContactDto],
+    })
+    async findAll() {
+        return this.contactService.findAll();
+    }
+}
--- backend-challenges/src/contact/contact.service.ts
+++ backend-challenges/src/contact/contact.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateContactDto } from './dto/create-contact.dto';
 
 @Injectable()
-export class ContactService {}
+export class ContactService {
+
+    private contacts: CreateContactDto[] = [];
+
+    async createContact(createContactDto: CreateContactDto) {
+        this.contacts.push(createContactDto);
+        const message = `Thank ${createContactDto.nome}, for getting in touch and sharing your interests. We look forward to hearing from you soon.`;
+        return { confirmation: message };
+    }
+
+    async findAll() {
+        return this.contacts;
+    }
+}
